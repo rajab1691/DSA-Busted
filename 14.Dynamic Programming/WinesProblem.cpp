49dc90899f100436569366c60ebb53cd151f856c
Raaj <48174962+rajab1691@users.noreply.github.com>
2022-01-20 19:08:28 +0530

    Create WinesProblem.cpp

---
+++ 14.Dynamic Programming/WinesProblem.cpp
@@ -0,0 +1,12 @@
+/*
+    Que: You can sell from extreme end and every next year price will be (year*price[i])
+         Find maximum profit.
+    eg: price[]={2,3,5,1,4}
+    o/p= 50 =(2*1+4*2+5*3+3*4+5*5)
+    
+    **We cannot apply greedy!!
+*/
+/*
+  1. Top-Down
+  T.C-O(n^2)
+*/
