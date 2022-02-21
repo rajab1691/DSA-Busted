0a16200aa4e6b12d7b47e8b1457d245ac3503fa6
Raaj <48174962+rajab1691@users.noreply.github.com>
2022-02-21 06:09:39 +0530

    Create ArrivalDep.cpp

---
+++ 15.Graph/3.DFS/ArrivalDep.cpp
@@ -0,0 +1,33 @@
+/*
+  1. Just maintain a global variable time
+*/
+
+void dfsUtil(int src, vector<int>&arrival,
+  vector<int>&dep, int &time,
+  vector<int>&visit) {
+
+  visit[src] = 1;
+  arrival[src] = ++time;
+
+  for (auto x: l[src]) {
+    if (visit[x] == 0) {
+      dfsUtil(x, arrival, dep, time, visit);
+    }
+  }
+  dep[src] = ++time;
+}
+void dfs(int src, int v) {
+    
+  vector<int>arrival(v,-1);
+  vector<int>dep(v,-1);
+  vector<int>visit(v,0);
+  int time = -1;
+
+  for (int i = 0; i <= v; i++)
+    if (visit[i] == 0)
+      dfsUtil(i, arrival, dep, time, visit);
+
+  for (int i = 0; i <= v; i++) {
+    cout << i << " " << arrival[i] << " " << dep[i] << endl;
+  }
+}
