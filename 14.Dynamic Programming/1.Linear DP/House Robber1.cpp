ef7354e0c0f1e87760c44e1af94f5cf7e81e5474
Raaj <48174962+rajab1691@users.noreply.github.com>
2022-03-23 09:45:51 +0530

    Update House Robber1.cpp

--- 14.Dynamic Programming/1.Linear DP/House Robber1.cpp
+++ 14.Dynamic Programming/1.Linear DP/House Robber1.cpp
@@ -58,3 +58,25 @@ int rob(vector<int>& nums) {
   }
   return dp[n - 1];
 }
+
+/*
+  Space Optimized Approach
+  T.C-O(n)
+  S.C-O(1)
+*/
+int rob(vector<int>&nums) {
+  int n = nums.size();
+  if (n == 1) return nums[0];
+  if (n == 2) return max(nums[0], nums[1]);
+
+  int prev2 = nums[0];
+  int prev1 = max(nums[0], nums[1]);
+  int curr;
+
+  for (int i = 2; i < n; i++) {
+    curr = max(prev1, prev2 + nums[i]);
+    prev2 = prev1;
+    prev1 = curr;
+  }
+  return curr;
+}
