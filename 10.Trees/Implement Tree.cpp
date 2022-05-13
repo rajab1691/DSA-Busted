2f0b0b589e108340c4a438eff2040e10333e1260
Raaj <48174962+rajab1691@users.noreply.github.com>
2022-05-13 08:13:21 +0530

    Update Implement Tree.cpp

--- 10.Trees/Implement Tree.cpp
+++ 10.Trees/Implement Tree.cpp
@@ -1,1 +1,29 @@
+class Node {
+public:
+	int val;
+	Node* left;
+	Node* right;
 
+	Node(int val) {
+		this->val = val;
+		left = right = NULL;
+	}
+};
+void printTreePre(Node* root) {
+
+	if (root == NULL)return;
+
+	cout << root->val << " ";
+	printTreePre(root->left);
+	printTreePre(root->right);
+}
+
+int32_t main() {
+
+	Node* root = new Node(1);
+
+	root->left = new Node(2);
+	root->right = new Node(3);
+
+	printTreePre(root);
+}
