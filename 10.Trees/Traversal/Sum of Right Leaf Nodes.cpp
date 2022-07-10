fcd9cb74e0c8cc9699b60ccdf7803d74f212005d
Raaj <48174962+rajab1691@users.noreply.github.com>
2022-07-10 11:06:46 +0530

    Create Sum of Right Leaf Nodes.cpp

---
+++ 10.Trees/Traversal/Sum of Right Leaf Nodes.cpp
@@ -0,0 +1,28 @@
+/*
+  Right leaf means two choice: do postorder or reverse inorder
+  
+*/
+bool isLeaf(Node* root) {
+	if (root and root->left == NULL and root->right == NULL)
+		return true;
+	return false;
+}
+Node* funcn(Node* root, int &sum) {
+	if (root == NULL)
+		return NULL;
+
+	if (root->right and isLeaf(root->right)) {
+		sum += root->right->data;
+	}
+	funcn(root->left, sum);
+	funcn(root->right, sum);
+	return root;
+}
+int rightLeafSum(Node* root)
+{
+	if (root == NULL)
+		return 0;
+	int sum = 0;
+	funcn(root, sum);
+	return sum;
+}
